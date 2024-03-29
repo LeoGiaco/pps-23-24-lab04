2c9d500b5a4587a01d5a99d86723c6b29625eec4
LeoGiaco <leogiaco2@gmail.com>
2024-03-29 15:46:43 +0100

    Complete task 5

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,8 @@
 package u04lab
 import u03.Sequences.* 
 import Sequence.*
+import u03.Optionals.Optional
+import Optional.*
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -22,5 +24,35 @@ object Ex5Traversable:
   def logAll[A](seq: Sequence[A]): Unit = seq match
     case Cons(h, t) => log(h); logAll(t)
     case _ => ()
+    
+  trait Traversable[T[_]]:
+    extension [A](el: T[A])
+      def consume(func: A => Unit): Unit
+
+  given Traversable[Optional] with
+    extension [A](el: Optional[A])
+      def consume(func: A => Unit): Unit = el match
+        case Optional.Just(v) => func(v)
+        case Optional.Empty() => ()
+  
+  given Traversable[Sequence] with
+    extension [A](el: Sequence[A])
+      def consume(func: A => Unit): Unit = el match
+        case Cons(h, t) => func(h); t.consume(func)
+        case Nil() => (9)
+    
+  @main def tryTraversables =
+    val seq1 = Cons("a", Cons("b", Cons("c", Cons("d", Nil()))))
+    seq1.consume(log)
+    val seq2 = Cons(10, Cons(20, Cons(30, Cons(40, Nil()))))
+    seq2.consume(log)
+    val opt1 = Just("prova")
+    opt1.consume(log)
+    val opt2 = Just(22)
+    opt2.consume(log)
+    val opt3 = Empty()
+    opt3.consume(log)
+
+
 
   
